2b6a6b4352e460fded2129ca5d02d597e20e9091
Shubh Agrawal <shubh@caltech.edu>
2021-04-13 10:56:26 -0400

    lab1 corrections

--- Physics 21/lab1/lab1.py
+++ Physics 21/lab1/lab1.py
@@ -1,18 +1,3 @@
-"""
-Physics 21, Assignment 1, Version 1.0
-Shubh Agrawal
-Class of 2022
-
-This .py file is my submission for Caltech's Physics 21.
-It accessses the online astrophysics data set for the Catalina survey,
-scraps data in html or VOTable format, and plots the data.
-"""
-
-import urllib.request
-import matplotlib.pyplot as plt
-from astropy.io.votable import parse_single_table
-
-
 # To allow for multiple figure to be printed in the same run
 figure_no = 0
 def plot(x, y, filename):
@@ -27,22 +12,40 @@ def plot(x, y, filename):
     plt.savefig(filename)
     figure_no = figure_no + 1
 
-url = "http://nesssi.cacr.caltech.edu/cgi-bin/getcssconedbid_release2.cgi"
 
-value = {"Name" : "Her X-1", "DB" : "photcal", "OUT" : "html", "SHORT" : "short"}
-data = urllib.parse.urlencode(value).encode('ascii')
+"""
+---- FEEDBACK ---- 
+(no action needed) 
 
-req = urllib.request.Request(url, data)
-with urllib.request.urlopen(req) as response:
-   page = response.read()
+You don't need to declare figure_no as a "global" to access it within plot() - it's global 
+(with respect to that function at least) because youy declared it outside the function.
 
-source = str(page.decode('ascii'))
+Also, you don't need to explicitly call plt.figure() with a figure number each time - you 
+can just call plt.figure() without an argument, or just call plt.scatter() without creating a 
+figure (pyplot automatically creates one in this case)
+
+Also, nice job making a doctsring!
+"""
+
+"""
+---- FEEDBACK ----
+(action needed)
+
+Having parts of your script floating between methods like this works, but it's not pretty.
+I would suggest consolidating the commands you want to execute into a "__main__" segment 
+at the end of the script, and keep all your functions before and outside of this "__main__" segment.
+It's much cleaner code, and good practice!
+"""
+
+
+
 
 def getValues(source):
     '''
     For Part 1:
         Gets values using html format
     '''
+    # find the start and end points of the data in the end file (indexes selected by hand)
     start = source.find('<td>1135075045477<td>13.34<td>0.05<td>254.4576')
     end = source.find('\n</table><br><p>\n<p><br><p></HTML>')
     vals = source[start: end]
@@ -54,22 +57,35 @@ def getValues(source):
     mag_len = len("13.34")
     mjd_len = len("53557.32593")
     pos = vals.find('1135075045477<td>') + leng
+    # lengths assigned by first values that are selected by hand
     
     while(pos >= leng):
+        # loop over the entire value set, and get all the mag and mjd
         mag.append(float(vals[pos: pos + mag_len]))
         mjd.append(float(vals[pos + diff: pos + diff + mjd_len]))
         vals = vals[pos + diff + mjd_len:]
         pos = vals.find('1135075045477<td>') + leng
         
     return (mag, mjd)
-
-mag, mjd = getValues(source)
     
-plot(mjd, mag, "html.png")
+    
+"""
+---- FEEDBACK ----
+(action needed)
 
-url = "http://nesssi.cacr.caltech.edu/DataRelease/upload/result_web_fileyUkAfF.vot"
+You should add a comment or two to this method (getValues()), to (briefly) explain what your code is
+doing 
+"""
 
-source = urllib.request.urlretrieve(url, filename="data.xml")
+"""
+---- FEEDBACK ----
+(action needed)
+
+Same comment as before about consolidating the floating parts into a "__main__" segment
+"""
+
+
+
 
 def getVOTvalues(filename):
     '''
@@ -86,6 +102,36 @@ def getVOTvalues(filename):
         mjd.append(item[0])
     return (mag, mjd)
     
-mag, mjd = getVOTvalues("data.xml")
 
-plot(mjd, mag, "vot.png")
+"""
+---- FEEDBACK ----
+(action needed)
+
+Same comment as before about consolidating the floating parts into a "__main__" segment
+"""
+
+if __name__ = "__main__":
+    url = "http://nesssi.cacr.caltech.edu/cgi-bin/getcssconedbid_release2.cgi"
+
+    value = {"Name" : "Her X-1", "DB" : "photcal", "OUT" : "html", "SHORT" : "short"}
+    data = urllib.parse.urlencode(value).encode('ascii')
+
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+       page = response.read()
+
+    source = str(page.decode('ascii'))
+
+    mag, mjd = getValues(source)
+    
+    plot(mjd, mag, "html.png")
+
+    url = "http://nesssi.cacr.caltech.edu/DataRelease/upload/result_web_fileyUkAfF.vot"
+
+    source = urllib.request.urlretrieve(url, filename="data.xml")
+
+    mag, mjd = getVOTvalues("data.xml")
+
+    plot(mjd, mag, "vot.png")
+
+
